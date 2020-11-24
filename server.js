f49fbfde1c6353f4491c5ccf9b79e8b966c7cbe4
Aleksandr4549 <63310470+Aleksandr4549@users.noreply.github.com>
2020-11-24 04:06:16 +0300

    Update and rename server.ts to server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
-import path from 'path';
 
 import ArticleController from './src/controllers/Article';
 import ArchitectureController from './src/controllers/Architecture';
 import StreetArtsController from './src/controllers/StreetArts';
+
+const path = require('path');
 
 dotenv.config();
 
@@ -18,7 +19,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use('/', express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
   });
 }
 
@@ -27,6 +28,6 @@ app.get('/article/:id', ArticleController.show);
 app.get('/architecture', ArchitectureController.index);
 app.get('/street-arts', StreetArtsController.index);
 
-app.listen(process.env.PORT || 5000, (): void => {
+app.listen(process.env.PORT || 8080, (): void => {
   console.log(`Server started: port ${process.env.PORT || 8080}`);
 });
